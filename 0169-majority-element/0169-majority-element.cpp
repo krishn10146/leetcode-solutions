18b64f40c47e3be97d70993c7f9117b4d9be5579
krishn10146 <krishnasharma10146@gmail.com>
2026-07-02 22:42:20 +0530

    Time: 0 ms (100%), Space: 28.3 MB (9.25%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& nums) {
+    int majorityElement(vector<int>& nums) { //moor's voting algorithm
         int n = nums.size();
-        int freq = 1,ans = nums[0];
+        int freq = 1,ans = nums[0];//initialising first elmnt of arr as ans
 
         for(int i = 1; i<n; i++){
             if(freq == 0){
-                ans = nums[i];
+                ans = nums[i];//updating ans everytime freq becomes 0
             }
-            if(ans == nums[i]){
+            if(ans == nums[i]){//if current no is same as previous
                 freq++;
             }else{
                 freq--;
