8b47f1b6fa29b424dc3214743baf707f1cc59f85
krishn10146 <krishnasharma10146@gmail.com>
2026-07-06 22:21:40 +0530

    Update 0125-valid-palindrome.cpp

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
-    bool isAlphaNum(char ch){ // function to check if the ch is alphanumneric which does not include *&^%$#@! etc.
-        if((ch >= '0' && ch <= '9') || (tolower(ch) >= 'a' && tolower(ch) <= 'z')){
-            return true;
-        }
-        return false;
-    }
 
     bool isPalindrome(string s) { // uses two pointer approach
         int n = s.length();
         int st = 0, e = n - 1;
         while(st<e){
-            if(!isAlphaNum(s[st])){ // checking if the ch is aphanum
+            if(!isalnum(s[st])){ // checking if the ch is aphanum  [alnum is an inbuit c++ funvtion to use this we must include <cctype>]
                 st++; continue;}
-            if(!isAlphaNum(s[e])){ // checking if the ch is aphanum
+            if(!isalnum(s[e])){ // checking if the ch is aphanum
                 e--; continue;}
             if(tolower(s[st]) == tolower(s[e])){ // to lower converts the ch to lowecase 
                 st++;
