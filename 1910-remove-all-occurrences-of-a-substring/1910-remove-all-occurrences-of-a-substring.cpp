f7f53af1a34c5462492ab9b049792b26a43814ac
krishn10146 <krishnasharma10146@gmail.com>
2026-07-07 21:44:04 +0530

    Refactor removeOccurrences function
    Removed commented return statement and cleaned up code.

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -4,7 +4,7 @@ public:
         int n = s.length();
         while(s.length() > 0 && s.find(part) < n){  //s.find(substring) function return the starting iteration of the substring we waanna find
                     s.erase(s.find(part),part.length());  // s.erase(str,end[also the length of the part we wanna remove]) erases the substring in the given substring iteration
-            // return s;
+           
         }
         return s;
     }
