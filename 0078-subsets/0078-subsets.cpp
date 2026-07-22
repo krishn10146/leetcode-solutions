588654f59f4ce41e9cdb435a7d6f9de5ec1cf53c
krishn10146 <krishnasharma10146@gmail.com>
2026-07-22 23:14:38 +0530

    Time: 0 ms (100%), Space: 10 MB (60.42%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
-    // helper function
+    // helper function      (using backtrackting to generate all subsets)
     void subsets(vector<int> &nums, vector<int> &ans, int i, vector<vector<int>> &storesubsets){
         int n = nums.size();
-            if(i ==  n){
-                storesubsets.push_back({ans});
+            if(i == n){                             // if (i == n) then it means that all the iterations or values are travelled
+                storesubsets.push_back({ans});      // so we pushback those elements in the vector ans
             return;
             
         }
-        
-        // include
-        ans.push_back(nums[i]);
-            subsets(nums, ans, i+1, storesubsets);
+        //for every iteration we have two options of INCLUDING and EXCLUDING
 
-        ans.pop_back();
+        // include
+        ans.push_back(nums[i]);                     // including current element to subset 
+            subsets(nums, ans, i+1, storesubsets);  // move to next step with item included
+
+         // (backtracking) removing last element to undo choice 
+        ans.pop_back();                            
         //exclude
+        //excluding the current element from subset
+        // move to next index without the item
             subsets(nums, ans, i+1, storesubsets);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> storesubsets;
-        vector<int> ans;
-        subsets(nums, ans, 0, storesubsets);
+        vector<vector<int>> storesubsets;           //  stores all the subsets in itself
+        vector<int> ans;                            //  tracks the current subset configuration
+        subsets(nums, ans, 0, storesubsets);        // starting the recoursive tracking the from index 0
         return storesubsets;
     }
 };
