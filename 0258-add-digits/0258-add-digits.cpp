33d1c1dbddca0647196190d75893a22764d3ee3e
krishn10146 <krishnasharma10146@gmail.com>
2026-07-23 12:38:51 +0530

    Time: 0 ms (100%), Space: 8.6 MB (80.54%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        
+        
+       while(num >= 10 )
+       {
+         
+        int digit = num%10;
+        num  = num/10;
+        num = digit + num;
+        if(num <10)
+        {
+            break;
+        }
+       }
+        return num;
+    }
+};
